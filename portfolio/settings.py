0e33862592267ee66195df0dbabda730c923d397
vatsal70 <68066033+vatsal70@users.noreply.github.com>
2021-08-05 16:13:50 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -184,7 +184,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #For sending Emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config('EMAIL_HOST') #'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER') #'studentteacher@hmail.com'
