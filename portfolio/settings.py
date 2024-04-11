22bf6725fbef79802b8858ca9d59073c811295da
Vatsal Vohera <68066033+vatsal70@users.noreply.github.com>
2024-04-11 14:04:41 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -200,5 +200,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST') #'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER') #'studentteacher@hmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') #'studentteacherportal'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
